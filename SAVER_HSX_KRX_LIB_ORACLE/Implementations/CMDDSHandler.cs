ea9d6ada3777d7eaa1a724790215b27b1447655d
KhanhNV <KhanhNV@fpts.com.vn>
2025-04-24 11:08:40 +0700

    sửa pub cho monitor

--- SAVER_HSX_KRX_LIB_ORACLE/Implementations/CMDDSHandler.cs
+++ SAVER_HSX_KRX_LIB_ORACLE/Implementations/CMDDSHandler.cs
@@ -173,16 +173,9 @@ namespace BaseSaverLib.Implementations
                 this._monitor.SendStatusToMonitor(
                 this._app.Common.GetLocalDateTime(),
                 this._app.Common.GetLocalIp(),
-                CMonitor.MONITOR_APP.HSX_Feeder5G_PT,
+                CMonitor.MONITOR_APP.HSX_Saver5G_Oracle,
                 arrMsg.Length,
                 SW_RD.ElapsedMilliseconds);
-
-                //this._monitor.SendStatusToMonitor(
-                //this._app.Common.GetLocalDateTime(),
-                //this._app.Common.GetLocalIp(),
-                //CMonitor.MONITOR_APP.HSX_Saver5G,
-                //arrMsg.Length,
-                //SW_RD.ElapsedMilliseconds);
 
                 return true;
             }
